fd25c5a8a69bba7dc37cf0c79360e9651ff69e4d
oak <oak>
2021-01-20 10:23:43 +0800

    install sdkman and gradle; install sdk

--- Dockerfile
+++ Dockerfile
@@ -12,18 +12,7 @@ RUN chmod a+x /opt/init.sh
 
 RUN /bin/bash -c /opt/init.sh
 
-RUN wget "https://dl.google.com/android/repository/commandlinetools-linux-6858069_latest.zip" -O /opt/sdk.zip
 
-RUN unzip /opt/sdk.zip -d /opt
 
-RUN wget https://downloads.gradle-dn.com/distributions/gradle-6.5-all.zip -O /opt/gradle-6.5-all.zip
-
-RUN /opt/cmdline-tools/bin/sdkmanager --sdk_root=/opt/android_sdk  --update  
-
-RUN /opt/cmdline-tools/bin/sdkmanager --sdk_root=/opt/android_sdk "platforms;android-25" "build-tools;25.0.2" "extras;google;m2repository" "extras;android;m2repository"
-
-RUN /bin/bash -c "yes | /opt/cmdline-tools/bin/sdkmanager --sdk_root=/opt/android_sdk  --licenses"
-
-RUN gradle wrapper --gradle-distribution-url file\:/opt/gradle-6.5-all.zip
 
 CMD ["touch", "/root/hello"]
--- shell/init.sh
+++ shell/init.sh
@@ -2,3 +2,18 @@
 curl -s "https://get.sdkman.io" | bash
 
 /bin/bash -c "source $HOME/.sdkman/bin/sdkman-init.sh; sdk install gradle"
+
+wget "https://dl.google.com/android/repository/commandlinetools-linux-6858069_latest.zip" -O /opt/sdk.zip
+
+unzip /opt/sdk.zip -d /opt
+
+wget https://downloads.gradle-dn.com/distributions/gradle-6.5-all.zip -O /opt/gradle-6.5-all.zip
+
+/opt/cmdline-tools/bin/sdkmanager --sdk_root=/opt/android_sdk  --update  
+
+/opt/cmdline-tools/bin/sdkmanager --sdk_root=/opt/android_sdk "platforms;android-25" "build-tools;25.0.2" "extras;google;m2repository" "extras;android;m2repository"
+
+yes | /opt/cmdline-tools/bin/sdkmanager --sdk_root=/opt/android_sdk  --licenses
+
+/bin/bash -c "source $HOME/.sdkman/bin/sdkman-init.sh; gradle wrapper --gradle-distribution-url file\:/opt/gradle-6.5-all.zip"
+
